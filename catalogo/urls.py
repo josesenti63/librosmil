d001cc2923ed7d1ebd6ff7add07ecc4cf7deda05
josesenti63 <88404469+josesenti63@users.noreply.github.com>
2022-08-04 12:56:00 -0300

    Actualizado 4 ago
    Act

--- catalogo/urls.py
+++ catalogo/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path 
+from django.urls import include, path 
 from django.contrib import admin
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.index, name='index'),
+    path('__debug__/', include('debug_toolbar.urls'),)
     ]
