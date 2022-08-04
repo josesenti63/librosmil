77dccd118b6bbff14eb5e83e3d488441d2a31095
josesenti63 <88404469+josesenti63@users.noreply.github.com>
2022-08-04 13:44:27 -0300

    debug
    Se agrego debug toolbar

--- librosmil/settings.py
+++ librosmil/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'debug_toolbar',
+    'registration',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,3 +131,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 INTERNAL_IPS = [
     "127.0.01",
 ]
+
+ACCOUNT_ACTIVATION_DAYS = 7
+REGISTRATION_AUTO_LOGIN = True
--- librosmil/urls.py
+++ librosmil/urls.py
@@ -19,4 +19,6 @@ from django.urls import include, path
 urlpatterns = [
     path('catalogo/', include('catalogo.urls')),
     path('admin/', admin.site.urls),
+    path('__debug__/', include('debug_toolbar.urls')),
+    path('accounts/', include('registration.backends.default.urls')),
 ]
