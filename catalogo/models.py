4bfca31aabc2cec8709b6594d688054ce3fbcddd
josesentinelli <jfsentinelli@yahoo.com.ar>
2022-08-04 10:52:19 -0300

    Se agrego tapa_libro a models.py

--- catalogo/models.py
+++ catalogo/models.py
@@ -14,6 +14,7 @@ class Libro(models.Model):
     titulo_libro = models.CharField(max_length=200)
     autor_libro = models.ForeignKey('Autor', on_delete=models.SET_NULL, null=True)
     # Se define ForeignKey porque un mismo autor escribe muchos libros
+    tapa_libro = models.ImageField(upload_to="catalogo/%Y/%m/%d", blank= True, null=True)
     sumario_libro = models.TextField(max_length=1000, help_text="Describa el libro")
     isbn_libro = models.CharField(max_length=13, help_text='13 Caracteres <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
 
